5faeebc8e52743ed2b98a72768de78dc94a8e533
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-06-15 14:54:48 +0430

    Demo if-else.

---
+++ src/lectures/demo/if-else/if-else.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func average(a, b, c int) float32 {
+	// Convert the sum of the scores into a float32
+	return float32(a+b+c) / 3
+}
+
+func main() {
+	quiz1, quiz2, quiz3 := 9, 7, 8
+
+	if quiz1 > quiz2 {
+		fmt.Println("quiz1 scored higher than quiz2")
+	} else if quiz1 < quiz2 {
+		fmt.Println("quiz2 scored higher than quiz1")
+	} else {
+		fmt.Println("quiz1 & quiz2 have the same score")
+	}
+
+	if grade := average(quiz1, quiz2, quiz3); grade > 7 {
+		fmt.Println("Acceptable grades")
+	} else {
+		fmt.Println("You suck")
+	}
+}
