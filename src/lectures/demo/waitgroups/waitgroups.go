aba3b454045a0ae61ee1006b70aa167d3a05590b
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-07-11 19:39:33 +0430

    Demo waitgroups

---
+++ src/lectures/demo/waitgroups/waitgroups.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	var wg sync.WaitGroup
+	counter := 0
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		counter++
+
+		go func() {
+			defer func() {
+				fmt.Println(counter, "goroutines remaining")
+				counter--
+				wg.Done()
+			}()
+
+			duration := time.Duration(rand.Intn(500) * int(time.Millisecond))
+			fmt.Println("Waiting for", duration)
+			time.Sleep(duration)
+		}()
+	}
+
+	fmt.Println("Waiting for goroutines to finish")
+	wg.Wait()
+	fmt.Println("done!")
+}
