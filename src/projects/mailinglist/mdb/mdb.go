fd0bc51b90c4282a1a19ead68ea4ca0b4a684f2d
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-11 14:46:14 +0430

    Mailing list: implement CRUD operation.

--- src/projects/mailinglist/mdb/mdb.go
+++ src/projects/mailinglist/mdb/mdb.go
@@ -14,6 +14,11 @@ type EmailEntry struct {
 	OptOut      bool
 }
 
+type GetEmailBatchQueryparams struct {
+	Page  int
+	Count int
+}
+
 func TryCreate(db *sql.DB) {
 	_, err := db.Exec(`
     CREATE TABLE emails (
@@ -57,4 +62,110 @@ func EmailEntryFromRow(row *sql.Rows) (*EmailEntry, error) {
 		ConfirmedAt: &t,
 		OptOut:      optOut,
 	}, nil
+}
+
+func CreateEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(
+		`
+    INSERT INTO emails (email, confirmed_at, opt_out)
+    VALUES (?, 0, false);
+    `,
+		email,
+	)
+
+	if err != nil {
+		log.Panicln(err)
+		return err
+	}
+
+	return nil
+}
+
+func GetEmail(db *sql.DB, email string) (*EmailEntry, error) {
+	rows, err := db.Query(`
+    SELECT id, email, confirmed_at, opt_out
+    FROM emails
+    WHERE email = ?
+  `, email)
+
+	defer rows.Close()
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	for rows.Next() {
+		return EmailEntryFromRow(rows)
+	}
+
+	return nil, nil
+}
+
+func UpdateEmail(db *sql.DB, entry EmailEntry) error {
+	t := entry.ConfirmedAt.Unix()
+
+	// NOTE: up-sert operation
+	_, err := db.Exec(`
+    INSERT INTO emails (email, confirmed_at, opt_out)
+    VALUES (?, ?, ?)
+    ON CONFLICT (email) DO UPDATE SET
+      confirmed_at = ?
+      opt_out = ?
+  `, entry.Email, t, entry.OptOut, t, entry.OptOut)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func DeleteEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+    UPDATE emails
+    SET opt_out = true
+    WHERE email = ?
+  `, email)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func GetEmailBatch(db *sql.DB, params GetEmailBatchQueryparams) ([]EmailEntry, error) {
+	var empty []EmailEntry
+
+	rows, err := db.Query(`
+    SELECT id, email, confirmed_at, opt_out
+    FROM emails
+    WHERE opt_out = false
+    ORDER BY id ASC
+    LIMIT ?
+    OFFSET ?
+  `,
+		params.Count, (params.Page-1)*params.Count,
+	)
+
+	if err != nil {
+		log.Println(err)
+		return empty, err
+	}
+	defer rows.Close()
+
+	emails := make([]EmailEntry, 0, params.Count)
+
+	for rows.Next() {
+		email, err := EmailEntryFromRow(rows)
+		if err != nil {
+			return nil, err
+		}
+		emails = append(emails, *email)
+	}
+
+	return emails, nil
 }
